15cfe9dce8d8e7b02c95b29683488b1ace62bd58
Swarup Ghosh <swghosh@redhat.com>
2023-05-09 17:41:27 +0530

    Support for cloud-agonistic implicit ambient mode credentials from env secret
    Signed-off-by: Swarup Ghosh <swghosh@redhat.com>

---
+++ pkg/controller/deployment/credentials_request.go
@@ -0,0 +1,147 @@
+package deployment
+
+import (
+	"fmt"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	coreinformersv1 "k8s.io/client-go/informers/core/v1"
+	"k8s.io/utils/pointer"
+
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cert-manager-operator/pkg/operator/operatorclient"
+	configinformersv1 "github.com/openshift/client-go/config/informers/externalversions/config/v1"
+
+	v1 "github.com/openshift/api/operator/v1"
+)
+
+type cloudCredentialsConfig struct {
+	mountDirectory string
+	mountFilename  string
+
+	secretKey string
+}
+
+const (
+	// credentials for AWS
+	awsCredentialsDir       = "/.aws"
+	awsCredentialsFileName  = "credentials"
+	awsCredentialsSecretKey = "credentials"
+
+	// credentials for GCP
+	gcpCredentialsDir       = "/.config/gcloud"
+	gcpCredentialsFileName  = "application_default_credentials.json"
+	gcpCredentialsSecretKey = "service_account.json"
+
+	// cloudCredentialsVolumeName is the volume name for mounting
+	// service account (gcp) or credentials (aws) file
+	cloudCredentialsVolumeName = "cloud-credentials"
+
+	// boundSA is the openshift bound service account
+	// containing the sts token
+	boundSATokenVolumeName = "bound-sa-token"
+	boundSATokenDir        = "/var/run/secrets/openshift/serviceaccount"
+	boundSAAudience        = "openshift"
+	boundSAPath            = "token"
+	boundSAExpirySec       = 3600
+)
+
+// currently supported cloud platforms for ambient credentialsare: AWS, GCP
+var cloudCredentialConfigs = map[configv1.PlatformType]cloudCredentialsConfig{
+	configv1.AWSPlatformType: {
+		mountDirectory: awsCredentialsDir,
+		mountFilename:  awsCredentialsFileName,
+
+		secretKey: awsCredentialsSecretKey,
+	},
+	configv1.GCPPlatformType: {
+		mountDirectory: gcpCredentialsDir,
+		mountFilename:  gcpCredentialsFileName,
+
+		secretKey: gcpCredentialsSecretKey,
+	},
+}
+
+func withCloudCredentials(secretsInformer coreinformersv1.SecretInformer, infraInformer configinformersv1.InfrastructureInformer, deploymentName, secretName string) func(operatorSpec *v1.OperatorSpec, deployment *appsv1.Deployment) error {
+	// cloud credentials is only required for the controller deployment,
+	// other deployments should be left untouched
+	if deploymentName != "cert-manager" {
+		return func(operatorSpec *v1.OperatorSpec, deployment *appsv1.Deployment) error {
+			return nil
+		}
+	}
+
+	return func(operatorSpec *v1.OperatorSpec, deployment *appsv1.Deployment) error {
+		volumes := []corev1.Volume{{
+			Name: boundSATokenVolumeName,
+			VolumeSource: corev1.VolumeSource{
+				Projected: &corev1.ProjectedVolumeSource{
+					DefaultMode: pointer.Int32(420),
+					Sources: []corev1.VolumeProjection{{
+						ServiceAccountToken: &corev1.ServiceAccountTokenProjection{
+							Audience:          boundSAAudience,
+							ExpirationSeconds: pointer.Int64(boundSAExpirySec),
+							Path:              boundSAPath,
+						}},
+					},
+				},
+			},
+		}}
+		volumeMounts := []corev1.VolumeMount{{
+			Name:      boundSATokenVolumeName,
+			MountPath: boundSATokenDir,
+			ReadOnly:  true,
+		}}
+
+		if len(secretName) > 0 {
+			_, err := secretsInformer.Lister().Secrets(operatorclient.TargetNamespace).Get(secretName)
+			if err != nil {
+				return err
+			}
+
+			if err != nil && apierrors.IsNotFound(err) {
+				return fmt.Errorf("(Retrying) cloud secret %q doesn't exist due to %v", secretName, err)
+			} else if err != nil {
+				return err
+			}
+
+			infra, err := infraInformer.Lister().Get("cluster")
+			if err != nil {
+				return err
+			}
+
+			cloudProvider := infra.Status.PlatformStatus.Type
+			if cloudCredentialConfig, ok := cloudCredentialConfigs[cloudProvider]; ok {
+				// supported cloud platform for mounting secrets found
+				volumes = append(volumes, corev1.Volume{
+					Name: cloudCredentialsVolumeName,
+					VolumeSource: corev1.VolumeSource{
+						Secret: &corev1.SecretVolumeSource{
+							SecretName: secretName,
+							Items: []corev1.KeyToPath{{
+								Key:  cloudCredentialConfig.secretKey,
+								Path: cloudCredentialConfig.mountFilename,
+							}},
+						},
+					},
+				})
+				volumeMounts = append(volumeMounts, corev1.VolumeMount{
+					Name:      cloudCredentialsVolumeName,
+					MountPath: cloudCredentialConfig.mountDirectory,
+				})
+			}
+		}
+
+		deployment.Spec.Template.Spec.Volumes = append(
+			deployment.Spec.Template.Spec.Volumes,
+			volumes...,
+		)
+		deployment.Spec.Template.Spec.Containers[0].VolumeMounts = append(
+			deployment.Spec.Template.Spec.Containers[0].VolumeMounts,
+			volumeMounts...,
+		)
+
+		return nil
+	}
+}
--- pkg/controller/deployment/generic_deployment_controller.go
+++ pkg/controller/deployment/generic_deployment_controller.go
@@ -4,6 +4,7 @@ import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 
+	configinformers "github.com/openshift/client-go/config/informers/externalversions"
 	"github.com/openshift/library-go/pkg/controller/factory"
 	"github.com/openshift/library-go/pkg/operator/deploymentcontroller"
 	"github.com/openshift/library-go/pkg/operator/events"
@@ -19,11 +20,13 @@ func newGenericDeploymentController(
 	controllerName, targetVersion, deploymentFile string,
 	operatorClient v1helpers.OperatorClientWithFinalizers,
 	certManagerOperatorInformers certmanoperatorinformers.SharedInformerFactory,
+	configInformer configinformers.SharedInformerFactory,
 	kubeClient kubernetes.Interface,
 	kubeInformersForTargetNamespace informers.SharedInformerFactory,
 	eventsRecorder events.Recorder,
 	versionRecorder status.VersionGetter,
 	trustedCAConfigmapName string,
+	cloucloudCredentialsSecretName string,
 ) factory.Controller {
 	deployment := resourceread.ReadDeploymentV1OrDie(assets.MustAsset(deploymentFile))
 	return deploymentcontroller.NewDeploymentController(
@@ -48,5 +51,6 @@ func newGenericDeploymentController(
 		withUnsupportedArgsOverrideHook,
 		withProxyEnv,
 		withCAConfigMap(kubeInformersForTargetNamespace.Core().V1().ConfigMaps(), deployment, trustedCAConfigmapName),
+		withCloudCredentials(kubeInformersForTargetNamespace.Core().V1().Secrets(), configInformer.Config().V1().Infrastructures(), deployment.Name, cloucloudCredentialsSecretName),
 	)
 }
